017edcce69f877e9cc9e9ef82caa14cf7c9d899e
Zeta0171 <santinozoccolini7@gmail.com>
2024-05-29 21:27:20 -0300

    Creacion de Product-list y productService e implementada la funcion getProductos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { NgModule } from "@angular/core";
+import { ProductListComponent } from "./product-list/product-list.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'listar', component: ProductListComponent}
   
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { UserService } from './services/user.service';
+import { ProductService } from './services/product.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ProductListComponent,
   ],
   imports: [
     BrowserModule,
@@ -20,7 +23,9 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     RouterModule
   ],
-  providers: [UserService],
+  providers: [UserService,
+              ProductService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
