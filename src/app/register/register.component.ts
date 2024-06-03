fba503931be3ebfe370ba27a01179f803a59f3ac
Santiago Gaston Cameto Hernandez <130415735+scameto@users.noreply.github.com>
2024-06-02 21:52:18 -0300

    Registro y login Exitoso

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+  submitted = false;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  
+  ) {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      role: ['', Validators.required],
+      telefono: ['', Validators.required]
+    });
+  }
+
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.userService.register(this.registerForm.value).subscribe(
+      data => {
+        console.log('User registered successfully!', data);
+        alert('Registro exitoso. Redirigiendo a la página de inicio de sesión.');
+        this.router.navigate(['/login']);
+      },
+      error => {
+        console.error('Error registering user', error);
+        alert('Error al registrar usuario. Por favor, inténtelo de nuevo.');
+      }
+    );
+  }
 }
