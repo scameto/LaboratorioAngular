63b5bdea7c1b51655a18458be2a95c8643c87ff3
Santiago Gaston Cameto Hernandez <130415735+scameto@users.noreply.github.com>
2024-07-05 00:55:50 -0300

    Ajuste de mensajes

--- src/app/services/pedidos.service.ts
+++ src/app/services/pedidos.service.ts
@@ -1,20 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class PedidoService {
-  obtenerPedidos() {
-    return JSON.parse(localStorage.getItem('pedidos') || '[]');
+  private apiUrl = 'http://localhost:3000/pedidos'; // Cambia esta URL por la URL de tu backend
+
+  constructor(private http: HttpClient) {}
+
+
+
+  obtenerPedidos(): Observable<any> {
+    return this.http.get(this.apiUrl);
   }
-
-  actualizarPedido(pedidoActualizado: any): void {
-    const pedidos = JSON.parse(localStorage.getItem('pedidos') || '[]');
-    const index = pedidos.findIndex((pedido: any) => pedido.id === pedidoActualizado.id);
-
-    if (index !== -1) {
-      pedidos[index] = pedidoActualizado;
-      localStorage.setItem('pedidos', JSON.stringify(pedidos));
-    }
+  
+  actualizarPedido(pedidoActualizado: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${pedidoActualizado.id}`, pedidoActualizado);
   }
 }
